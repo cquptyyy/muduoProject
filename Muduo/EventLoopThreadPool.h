0aa099761a838d3f09b23fc567fff53feb3f6103
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:10:54 +0800

    Update EventLoopThreadPool.h

--- Muduo/EventLoopThreadPool.h
+++ Muduo/EventLoopThreadPool.h
@@ -1,40 +1,40 @@
-#ifndef MUDUO_BASE_EVENTLOOPTHREADPOOL_H
-#define MUDUO_BASE_EVENTLOOPTHREADPOOL_H
-
+#pragma once
 #include "noncopyable.h"
+
+#include <functional>
 #include <string>
 #include <vector>
 #include <memory>
-#include <functional>
 
 class EventLoop;
 class EventLoopThread;
 
-class EventLoopThreadPool{
+class EventLoopThreadPool : noncopyable
+{
 public:
-  using ThreadInitCallback=std::function<void(EventLoop*)>;
+    using ThreadInitCallback = std::function<void(EventLoop*)>; 
 
-  EventLoopThreadPool(EventLoop* baseloop,const std::string& nameArg);
-  ~EventLoopThreadPool();
+    EventLoopThreadPool(EventLoop *baseLoop, const std::string &nameArg);
+    ~EventLoopThreadPool();
 
-  void setThreadNum(int numThreads){numThreads_=numThreads;}
-  void start(const ThreadInitCallback& cb=ThreadInitCallback());
+    void setThreadNum(int numThreads) { numThreads_ = numThreads; }
 
-  //获取下一个channel分配的loop
-  EventLoop* getNextLoop();
+    void start(const ThreadInitCallback &cb = ThreadInitCallback());
 
-  //获取所有线程执行的loop
-  std::vector<EventLoop*> getAllLoops();
-  bool started()const{return started_;}
-  const std::string name()const{return name_;}
+    // 如果工作在多线程中，baseLoop_默认以轮询的方式分配channel给subloop
+    EventLoop* getNextLoop();
+
+    std::vector<EventLoop*> getAllLoops();
+
+    bool started() const { return started_; }
+    const std::string name() const { return name_; }
 private:
-  EventLoop* baseloop_;
-  std::string name_;
-  bool started_;//线程池是否启动
-  int numThreads_;//线程池中线程的数量
-  int next_;//下一派发channel的线程下标
-  std::vector<std::unique_ptr<EventLoopThread>> threads_;//所有线程
-  std::vector<EventLoop*> loops_;//所有线程执行的所有事件循环
-};
 
-#endif
+    EventLoop *baseLoop_; // EventLoop loop;  
+    std::string name_;
+    bool started_;
+    int numThreads_;
+    int next_;
+    std::vector<std::unique_ptr<EventLoopThread>> threads_;
+    std::vector<EventLoop*> loops_;
+};
