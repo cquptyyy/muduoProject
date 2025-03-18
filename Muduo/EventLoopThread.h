80c644550bc2b2d375b2032f4e3c4ca8dedd2803
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:10:18 +0800

    Update EventLoopThread.h

--- Muduo/EventLoopThread.h
+++ Muduo/EventLoopThread.h
@@ -1,36 +1,32 @@
-#ifndef MUDUO_BASE_EVENTLOOPTHREAD_H
-#define MUDUO_BASE_EVENTLOOPTHREAD_H
+#pragma once
 
-#include <mutex>
-#include <functional>
-#include <condition_variable>
 #include "noncopyable.h"
 #include "Thread.h"
 
-/**
- * 封装一个thread和一个loop，每个thread在线程函数中启动一个loop
-*/
-
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <string>
 
 class EventLoop;
 
-class EventLoopThread:noncopyable{
+class EventLoopThread : noncopyable
+{
 public:
-  using ThreadInitCallback=std::function<void(EventLoop*)>;
+    using ThreadInitCallback = std::function<void(EventLoop*)>; 
 
-  EventLoopThread(const ThreadInitCallback& cb=ThreadInitCallback(),const std::string& name=std::string());
-  ~EventLoopThread();
-  //启动线程，
-  EventLoop* startLoop();
+    EventLoopThread(const ThreadInitCallback &cb = ThreadInitCallback(), 
+        const std::string &name = std::string());
+    ~EventLoopThread();
+
+    EventLoop* startLoop();
 private:
-  void threadFunc();
+    void threadFunc();
 
-  EventLoop* loop_;//事件循环
-  bool exiting_;//标识事件循环结束，线程结束
-  Thread thread_;//用于执行loop循环的thread
-  std::mutex mutex_;//用于线程通信，返回loop时，loop必须已经创建成功
-  std::condition_variable cond_;//用于线程通信
-  ThreadInitCallback callback_;//初始化loop
+    EventLoop *loop_;
+    bool exiting_;
+    Thread thread_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    ThreadInitCallback callback_;
 };
-
-#endif
