e157385b4e2209899c5e09e4ac42859898fdacbf
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:15:45 +0800

    Update Thread.h

--- Muduo/Thread.h
+++ Muduo/Thread.h
@@ -1,39 +1,38 @@
-#ifndef MUDUO_BASE_THREAD_H
-#define MUDUO_BASE_THREAD_H
+#pragma once
 
-#include <atomic>
-#include <thread>
-#include <functional>
-#include <string>
-#include <memory>
 #include "noncopyable.h"
 
-class Thread:noncopyable
+#include <functional>
+#include <thread>
+#include <memory>
+#include <unistd.h>
+#include <string>
+#include <atomic>
+
+class Thread : noncopyable
 {
 public:
-  using ThreadFunc=std::function<void()>;
+    using ThreadFunc = std::function<void()>;
 
-  explicit Thread(ThreadFunc,const std::string &name=std::string());
-  ~Thread();
-  
-  void start();
-  void join();
-  
-  bool started()const{return started_;};
-  pid_t tid()const{return tid_;}
-  const std::string& name()const{return name_;}
-  
-  static int numCreated(){return numCreated_;}
+    explicit Thread(ThreadFunc, const std::string &name = std::string());
+    ~Thread();
+
+    void start();
+    void join();
+
+    bool started() const { return started_; }
+    pid_t tid() const { return tid_; }
+    const std::string& name() const { return name_; }
+
+    static int numCreated() { return numCreated_; }
 private:
-  void setDefaultName();
+    void setDefaultName();
 
-  bool started_;
-  bool joined_;
-  std::shared_ptr<std::thread> thread_; 
-  pid_t tid_;
-  std::string name_;
-  ThreadFunc func_;
-  static std::atomic_int numCreated_;
+    bool started_;
+    bool joined_;
+    std::shared_ptr<std::thread> thread_;
+    pid_t tid_;
+    ThreadFunc func_;
+    std::string name_;
+    static std::atomic_int numCreated_;
 };
-
-#endif
