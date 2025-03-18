36f547d49e25beba8164cda3164c9a1f348918fd
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:05:43 +0800

    Update Channel.h

--- Muduo/Channel.h
+++ Muduo/Channel.h
@@ -1,90 +1,83 @@
-#ifndef MUDUO_BASE_CHANNEL_H
-#define MUDUO_BASE_CHANNEL_H
+#pragma once
 
-/** 
- * Channel封装了文件描述符fd，用户感兴趣的事件event，
- * poller返回的就绪的事件，以及处理不同事件的回调函数
-*/
-
+#include "noncopyable.h"
+#include "Timestamp.h"
 
 #include <functional>
 #include <memory>
-#include "noncopyable.h"
-#include "TimeStamp.h"
 
-class EventLoop; 
+class EventLoop;
 
-class Channel:noncopyable
+/**
+ * 理清楚  EventLoop、Channel、Poller之间的关系   《= Reactor模型上对应 Demultiplex
+ * Channel 理解为通道，封装了sockfd和其感兴趣的event，如EPOLLIN、EPOLLOUT事件
+ * 还绑定了poller返回的具体事件
+ */ 
+class Channel : noncopyable
 {
 public:
-  using EventCallback=std::function<void()>;
-  using ReadEventCallback=std::function<void(TimeStamp)>;
+    using EventCallback = std::function<void()>;
+    using ReadEventCallback = std::function<void(Timestamp)>;
 
-  Channel(EventLoop* loop,int fd);
-  ~Channel();
+    Channel(EventLoop *loop, int fd);
+    ~Channel();
 
-  //fd有事件就绪poller通知channel处理就绪事件
-  void handleEvent(TimeStamp receiveTime);
+    // fd得到poller通知以后，处理事件的
+    void handleEvent(Timestamp receiveTime);  
 
-  //设置fd对应的事件处理函数
-  void setReadCallback(ReadEventCallback cb){readCallback_=std::move(cb);}
-  void setWriteCallback(EventCallback cb){writeCallback_=std::move(cb);}
-  void setErrorCallback(EventCallback cb){errorCallback_=std::move(cb);}
-  void setCloseCallback(EventCallback cb){closeCallback_=std::move(cb);}
+    // 设置回调函数对象
+    void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
+    void setWriteCallback(EventCallback cb) { writeCallback_ = std::move(cb); }
+    void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
+    void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
 
-  //防止channel已经被删除还在处理事件
-  void tie(const std::shared_ptr<void>&);
+    // 防止当channel被手动remove掉，channel还在执行回调操作
+    void tie(const std::shared_ptr<void>&);
 
-  //返回fd关心的事件
-  int events()const{return events_;}
-  int fd()const{return fd_;}
+    int fd() const { return fd_; }
+    int events() const { return events_; }
+    int set_revents(int revt) { revents_ = revt; }
 
-  //设置fd已经就绪的事件
-  void set_revents(int revents){revents_=revents;}
+    // 设置fd相应的事件状态
+    void enableReading() { events_ |= kReadEvent; update(); }
+    void disableReading() { events_ &= ~kReadEvent; update(); }
+    void enableWriting() { events_ |= kWriteEvent; update(); }
+    void disableWriting() { events_ &= ~kWriteEvent; update(); }
+    void disableAll() { events_ = kNoneEvent; update(); }
 
-  //设置fd关心的事件
-  void enableReading(){events_|=kReadEvent_;update();}
-  void disableReading(){events_&=~kReadEvent_;update();}
-  void enableWriting(){events_|=kWriteEvent_;update();}
-  void disableWriting(){events_&=~kWriteEvent_;update();}
-  void disableAll(){events_=kNonEvent_;update();}
+    // 返回fd当前的事件状态
+    bool isNoneEvent() const { return events_ == kNoneEvent; }
+    bool isWriting() const { return events_ & kWriteEvent; }
+    bool isReading() const { return events_ & kReadEvent; }
 
-  //返回fd对应的事件是否就绪
-  bool isReading()const {return events_&kReadEvent_;}
-  bool isWriting()const {return events_&kWriteEvent_;}
-  bool isNonEvent()const{return events_==kNonEvent_;}
+    int index() { return index_; }
+    void set_index(int idx) { index_ = idx; }
 
-  int index()const{return index_;}
-  void set_index(int index){index_=index;}
-
-  EventLoop* ownerLoop()const{return loop_;}
-  void remove();
-
+    // one loop per thread
+    EventLoop* ownerLoop() { return loop_; }
+    void remove();
 private:
-  //更新poller应该关心的事件
-  void update();
-  void handleEventWithGuard(TimeStamp receiveTime);
 
+    void update();
+    void handleEventWithGuard(Timestamp receiveTime);
 
-  const static int kNonEvent_;
-  const static int kReadEvent_;
-  const static int kWriteEvent_;
+    static const int kNoneEvent;
+    static const int kReadEvent;
+    static const int kWriteEvent;
 
-  EventLoop* loop_;
-  const int fd_;
-  int events_;//用户关心的事件 如EPOLLIN EPOLLOUT
-  int revents_;//poller返回的就绪事件
-  int index_;//用于记录channel在epoll中的状态
+    EventLoop *loop_; // 事件循环
+    const int fd_;    // fd, Poller监听的对象
+    int events_; // 注册fd感兴趣的事件
+    int revents_; // poller返回的具体发生的事件
+    int index_;
 
-  std::weak_ptr<void> tie_;
-  bool tied_;
-  
+    std::weak_ptr<void> tie_;
+    bool tied_;
 
-  //事件对应的回调函数
-  ReadEventCallback readCallback_;
-  EventCallback writeCallback_;
-  EventCallback errorCallback_;
-  EventCallback closeCallback_;
+    // 因为channel通道里面能够获知fd最终发生的具体的事件revents，所以它负责调用具体事件的回调操作
+    ReadEventCallback readCallback_;
+    EventCallback writeCallback_;
+    EventCallback closeCallback_;
+    EventCallback errorCallback_;
 };
 
-#endif
