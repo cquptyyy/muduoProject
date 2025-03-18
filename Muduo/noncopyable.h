120b0e87f0c7b8a17b678d02934165e4026b639f
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:17:44 +0800

    Update noncopyable.h

--- Muduo/noncopyable.h
+++ Muduo/noncopyable.h
@@ -1,15 +1,18 @@
 #ifndef MUDUO_BASE_NONCOPYABLE_H
 #define MUDUO_BASE_NONCOPYABLE_H
-/*
-继承noncopyable的派生类不能拷贝构造和赋值，但可以析构和普通构造
-*/
-class noncopyable{
-public:
-  noncopyable(const noncopyable&)=delete;
-  noncopyable& operator=(const noncopyable&)=delete;
-protected:
-  noncopyable()=default;
-  ~noncopyable()=default;
-};
 
+
+/**
+ * noncopyable被继承以后，派生类对象可以正常的构造和析构，但是派生类对象
+ * 无法进行拷贝构造和赋值操作
+ */ 
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+};
 #endif 
