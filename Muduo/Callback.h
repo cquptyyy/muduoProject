c2b743d331995a71cbfe5bdc63a8e62bfc84f5d5
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:05:03 +0800

    Update Callback.h

--- Muduo/Callback.h
+++ Muduo/Callback.h
@@ -1,19 +1,17 @@
-#ifndef MUDUO_BASE_CALLBACK_H
-#define MUDUO_BASE_CALLBACK_H
+#pragma once
 
 #include <memory>
 #include <functional>
 
 class Buffer;
 class TcpConnection;
-class TimeStamp;
+class Timestamp;
 
-using TcpConnectionPtr=std::shared_ptr<TcpConnection>;//tcp连接
-using ConnectionCallback=std::function<void(const TcpConnectionPtr&)>;//新连接的回调函数
-using CloseCallback=std::function<void(const TcpConnectionPtr&)>;//关闭连接的回调函数
-using WriteCompleteCallback=std::function<void(const TcpConnectionPtr&)>;//写事件的回调函数
-using MessageCallback=std::function<void(const TcpConnectionPtr&,Buffer*,TimeStamp)>;//读事件就绪的回调函数
-using HighWaterMarkCallback=std::function<void(const TcpConnectionPtr&,size_t)>;
-
-
-#endif
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+using ConnectionCallback = std::function<void (const TcpConnectionPtr&)>;
+using CloseCallback = std::function<void (const TcpConnectionPtr&)>;
+using WriteCompleteCallback = std::function<void (const TcpConnectionPtr&)>;
+using MessageCallback = std::function<void (const TcpConnectionPtr&,
+                                        Buffer*,
+                                        Timestamp)>;
+using HighWaterMarkCallback = std::function<void (const TcpConnectionPtr&, size_t)>;
