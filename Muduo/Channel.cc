ec915837380f137828e3fffe801a648142edfdfd
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:05:27 +0800

    Update Channel.cc

--- Muduo/Channel.cc
+++ Muduo/Channel.cc
@@ -1,67 +1,97 @@
 #include "Channel.h"
+#include "EventLoop.h"
+#include "Logger.h"
 
 #include <sys/epoll.h>
 
-const int Channel::kNonEvent_=0;
-const int Channel::kReadEvent_=EPOLLIN|EPOLLPRI;
-const int Channel::kWriteEvent_=EPOLLOUT;
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
 
-Channel::Channel(EventLoop* loop,int fd)
-  :loop_(loop),fd_(fd),events_(0),revents_(0),index_(-1),tied_(false)
-  {}
-
-Channel::~Channel(){}
-
-//fd对应的事件已更新，需要从新注册进poller中
-void Channel::update(){
-  //loop_->updateChannel(this);
+// EventLoop: ChannelList Poller
+Channel::Channel(EventLoop *loop, int fd)
+    : loop_(loop), fd_(fd), events_(0), revents_(0), index_(-1), tied_(false)
+{
 }
 
-
-//将fd对应的channel删除
-void Channel::remove(){
-  //loop_->removeChannel(this);
+Channel::~Channel()
+{
 }
 
-//什么时候调用tie
-void Channel::tie(const std::shared_ptr<void>& obj){
-  tie_=obj;
-  tied_=true;
+// channel的tie方法什么时候调用过？一个TcpConnection新连接创建的时候 TcpConnection => Channel 
+void Channel::tie(const std::shared_ptr<void> &obj)
+{
+    tie_ = obj;
+    tied_ = true;
 }
 
-//fd有事件就绪，调用事件处理函数
-void Channel::handleEvent(TimeStamp receiveTime){
-  if(tied_==true){
-    std::shared_ptr<void> guard=tie_.lock();
-    if(guard!=nullptr){
-      handleEventWithGuard(receiveTime);
-    }
-  }
-  else{
-    handleEventWithGuard(receiveTime);
-  }
+/**
+ * 当改变channel所表示fd的events事件后，update负责在poller里面更改fd相应的事件epoll_ctl
+ * EventLoop => ChannelList   Poller
+ */ 
+void Channel::update()
+{
+    // 通过channel所属的EventLoop，调用poller的相应方法，注册fd的events事件
+    loop_->updateChannel(this);
 }
 
-//对不同事件调用不同的回调函数
-void Channel::handleEventWithGuard(TimeStamp receiveTime){
-  if((events_&EPOLLHUP)&!(events_&EPOLLIN)){
-    if(closeCallback_!=nullptr){
-      closeCallback_();
+// 在channel所属的EventLoop中， 把当前的channel删除掉
+void Channel::remove()
+{
+    loop_->removeChannel(this);
+}
+
+// fd得到poller通知以后，处理事件的
+void Channel::handleEvent(Timestamp receiveTime)
+{
+    if (tied_)
+    {
+        std::shared_ptr<void> guard = tie_.lock();
+        if (guard)
+        {
+            handleEventWithGuard(receiveTime);
+        }
     }
-  }
-  if(events_&EPOLLERR){
-    if(errorCallback_!=nullptr){
-      errorCallback_();
+    else
+    {
+        handleEventWithGuard(receiveTime);
     }
-  }
-  if(events_&EPOLLOUT){
-    if(writeCallback_!=nullptr){
-      writeCallback_();
+}
+
+// 根据poller通知的channel发生的具体事件， 由channel负责调用具体的回调操作
+void Channel::handleEventWithGuard(Timestamp receiveTime)
+{
+    LOG_INFO("channel handleEvent revents:%d\n", revents_);
+
+    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN))
+    {
+        if (closeCallback_)
+        {
+            closeCallback_();
+        }
     }
-  }
-  if(events_&(EPOLLIN|EPOLLPRI)){
-    if(readCallback_!=nullptr){
-      readCallback_(receiveTime);
+
+    if (revents_ & EPOLLERR)
+    {
+        if (errorCallback_)
+        {
+            errorCallback_();
+        }
     }
-  }
+
+    if (revents_ & (EPOLLIN | EPOLLPRI))
+    {
+        if (readCallback_)
+        {
+            readCallback_(receiveTime);
+        }
+    }
+
+    if (revents_ & EPOLLOUT)
+    {
+        if (writeCallback_)
+        {
+            writeCallback_();
+        }
+    }
 }
