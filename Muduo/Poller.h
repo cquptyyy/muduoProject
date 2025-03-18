25a5b785c6f8e578f0ade34255c0d18bc7e7cdcc
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:12:53 +0800

    Update Poller.h

--- Muduo/Poller.h
+++ Muduo/Poller.h
@@ -1,42 +1,37 @@
-#ifndef MUDUO_BASE_POLLER_H
-#define MUDUO_BASE_POLLER_H
+#pragma once
+
+#include "noncopyable.h"
+#include "Timestamp.h"
 
 #include <vector>
 #include <unordered_map>
-#include "noncopyable.h"
-#include "TimeStamp.h"
 
 class Channel;
 class EventLoop;
 
-class Poller:noncopyable
+// muduo库中多路事件分发器的核心IO复用模块
+class Poller : noncopyable
 {
 public:
-  using ChannelList=std::vector<Channel*>;
+    using ChannelList = std::vector<Channel*>;
 
-  Poller(EventLoop* loop);
-  virtual ~Poller()=default;
+    Poller(EventLoop *loop);
+    virtual ~Poller() = default;
 
-  //启动IO复用监视文件描述符的就绪状态
-  virtual TimeStamp poll(int timeoutMs,ChannelList* activeChannels)=0;
-  //更新IO复用监视的文件描述符
-  virtual void updateChannel(Channel* channel)=0;
-  //从IO复用监视的channels中删除对应的channel即不再监视channel中的文件描述符
-  virtual void removeChannel(Channel* channel)=0;
+    // 给所有IO复用保留统一的接口
+    virtual Timestamp poll(int timeoutMs, ChannelList *activeChannels) = 0;
+    virtual void updateChannel(Channel *channel) = 0;
+    virtual void removeChannel(Channel *channel) = 0;
+    
+    // 判断参数channel是否在当前Poller当中
+    bool hasChannel(Channel *channel) const;
 
-  //判断的文件描述符是否被监视
-  bool hasChannel(Channel* channel)const;
-
-  //获取IO复用的对象
-  static Poller* newDefaultPoller(EventLoop* loop);
-
+    // EventLoop可以通过该接口获取默认的IO复用的具体实现
+    static Poller* newDefaultPoller(EventLoop *loop);
 protected:
-//key对应为sockfd value为sockfd对应的channel
-  using ChannelMap=std::unordered_map<int,Channel*>;
-  ChannelMap channels_;
-
+    // map的key：sockfd  value：sockfd所属的channel通道类型
+    using ChannelMap = std::unordered_map<int, Channel*>;
+    ChannelMap channels_;
 private:
-  EventLoop* ownerLoop_;
+    EventLoop *ownerLoop_; // 定义Poller所属的事件循环EventLoop
 };
-
-#endif
