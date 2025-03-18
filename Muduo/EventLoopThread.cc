e5bdd9d1c2955918c92784e2877718081496d335
cquptyyy <148693661+cquptyyy@users.noreply.github.com>
2025-03-18 13:09:57 +0800

    Update EventLoopThread.cc

--- Muduo/EventLoopThread.cc
+++ Muduo/EventLoopThread.cc
@@ -2,54 +2,61 @@
 #include "EventLoop.h"
 
 
-//传入线程函数初始化一个线程，获取初始化loop的对象
-EventLoopThread::EventLoopThread(const ThreadInitCallback& cb,const std::string&name)
-  :callback_(cb)
-  ,thread_(std::bind(EventLoopThread::threadFunc,this),name)
-  ,loop_(nullptr)
-  ,exiting_(false)
-  ,mutex_()
-  ,cond_()
-  {}
+EventLoopThread::EventLoopThread(const ThreadInitCallback &cb, 
+        const std::string &name)
+        : loop_(nullptr)
+        , exiting_(false)
+        , thread_(std::bind(&EventLoopThread::threadFunc, this), name)
+        , mutex_()
+        , cond_()
+        , callback_(cb)
+{
 
-
-//将事件循环停止，等待线程结束
-EventLoopThread::~EventLoopThread(){
-  exiting_=true;
-  if(loop_!=nullptr){
-    loop_->quit();
-    thread_.join();
-  }
 }
 
-
-//启动线程，开始事件循环，获取loop指针
-EventLoop* EventLoopThread::startLoop(){
-  thread_.start();
-  EventLoop* loop=nullptr;
-  {
-    std::unique_lock<std::mutex> lock(mutex_);
-    while(loop_==nullptr){
-      cond_.wait(lock);
+EventLoopThread::~EventLoopThread()
+{
+    exiting_ = true;
+    if (loop_ != nullptr)
+    {
+        loop_->quit();
+        thread_.join();
     }
-    loop=loop_;
-  }
-  return loop;
 }
 
+EventLoop* EventLoopThread::startLoop()
+{
+    thread_.start(); // 启动底层的新线程
 
-//线程执行的线程函数
-void EventLoopThread::threadFunc(){
-  EventLoop loop;
-  if(callback_!=nullptr){
-    callback_(&loop);
-  }
-  {
+    EventLoop *loop = nullptr;
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        while ( loop_ == nullptr )
+        {
+            cond_.wait(lock);
+        }
+        loop = loop_;
+    }
+    return loop;
+}
+
+// 下面这个方法，实在单独的新线程里面运行的
+void EventLoopThread::threadFunc()
+{
+    EventLoop loop; // 创建一个独立的eventloop，和上面的线程是一一对应的，one loop per thread
+
+    if (callback_)
+    {
+        callback_(&loop);
+    }
+
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        loop_ = &loop;
+        cond_.notify_one();
+    }
+
+    loop.loop(); // EventLoop loop  => Poller.poll
     std::unique_lock<std::mutex> lock(mutex_);
-    loop_=&loop;
-    cond_.notify_one();
-  }
-  loop.loop();
-  std::unique_lock<std::mutex> lock(mutex_);
-  loop_=nullptr;
+    loop_ = nullptr;
 }
